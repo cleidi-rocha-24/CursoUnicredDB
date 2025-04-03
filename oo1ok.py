0426448e475d6db0cccd893922b76655bba978a0
Cleidi Rocha da Silva - Unicred do Brasil <cleidi.silva@e-unicred.com.br>
2025-04-03 12:57:04 -0300

    fix: correcoes gerais em oo

--- oo1ok.py
+++ oo1ok.py
@@ -1,5 +1,38 @@
 import json
 
+class Produto:
+    def __init__(self, id, nome, quantidade, preco):
+        self.id = id
+        self.nome = nome
+        self.quantidade = quantidade
+        self.preco = preco
+        
+class Mercado:
+    def __init__(self):
+        self.produto = []
+
+    def adicionar_produtos(self, itens_adicionado): 
+        self.produto.append(itens_adicionado) 
+       
+    def exibir_lista_produtos(self):
+       for produtos in self.produto:
+            id = produtos.id
+            nome = produtos.nome
+            quantidade = produtos.quantidade
+            preco = produtos.preco
+                      
+            print(f'ID: {id}, Produto: {nome}, Quantidade: {quantidade}, Preço Unitário: R${preco:.2f}')
+                 
+    def valor_e_quantidade_total_produto(self):
+        quantidade_total = 0
+        valor_total_produto = 0
+        for produtos in self.produto:
+            quantidade_total += produtos.quantidade       
+            valor_total_produto += produtos.quantidade * produtos.preco
+        print(f'Quantidade total dos produtos no mercado:{quantidade_total}')
+        print(f'Valor total dos produtos no mercado: R${valor_total_produto:.2f}')
+
+
 produto_json = '''
 [
     { "id": 1, "nome": "Arroz", "quantidade": 50, "preco": 20.99 },
@@ -11,45 +44,22 @@ produto_json = '''
     { "id": 7, "nome": "Óleo de Soja", "quantidade": 70, "preco": 9.50 },
     { "id": 8, "nome": "Sabonete", "quantidade": 200, "preco": 1.50 },
     { "id": 9, "nome": "Detergente", "quantidade": 150, "preco": 2.30 },
-    { "id": 10, "nome": "Papel Higiênico", "quantidade": 80, "preco": 12.00 }
+    { "id": 10, "nome": "Papel Higiênico", "quantidade": 90, "preco": 22.00 }
 ]
 '''
-
-class Mercado:
-    def __init__(self):
-        self.produtos = json.loads(produto_json)
-
-    def adicionar_produtos(self, nome_produto, quantidade, preco): 
-        produto = {             
-            'nome': nome_produto,             
-            'quantidade': quantidade,             
-            'preco': preco 
-        } 
-        self.produtos.append(produto) 
-       
-    def exibir_lista_produtos(self):
-       for produto in self.produtos:
-            nome = produto["nome"]
-            quantidade = produto["quantidade"]
-            preco = produto["preco"]
-                      
-            print(f'Produto: {nome}, Quantidade: {quantidade}, Preço Unitário: R${preco:.2f}')
-                 
-    def valor_e_quantidade_total_produto(self):
-        quantidade_total = 0
-        valor_total_produto = 0
-        for produto in self.produtos:
-            quantidade_total += produto["quantidade"]       
-            valor_total_produto += produto["quantidade"] * produto["preco"]
-            #valor_total = valor_total + valor_total_produto 
-        print(f'Quantidade total dos produtos no mercado:{quantidade_total}')
-        print(f'Valor total dos produtos no mercado: R${valor_total_produto:.2f}')
-
+produtosArquivo = json.loads(produto_json)
+mercado = Mercado()
+for item in produtosArquivo:
+    idProduto = item["id"]
+    nomeProduto = item["nome"]
+    quantProduto = item ["quantidade"]
+    precoProduto = item ["preco"]
     
-produto = Mercado()
-produto.adicionar_produtos("nome_produto_tst",2,2.50) #ajustar para possibilitar que usuário digite
-produto.exibir_lista_produtos()
-produto.valor_e_quantidade_total_produto()
+    produto = Produto(idProduto,nomeProduto,quantProduto,precoProduto)
+    mercado.adicionar_produtos(produto)
+
+mercado.exibir_lista_produtos()
+mercado.valor_e_quantidade_total_produto()
 
 
     
